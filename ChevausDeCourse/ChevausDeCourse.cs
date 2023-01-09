689dcf1713ee73e06d5a652ffd7ee94ae4bb856d
SylvainTorrenti <torrsyl@gmail.com>
2023-01-09 17:02:49 +0100

    exercice

--- ChevausDeCourse/ChevausDeCourse.cs
+++ ChevausDeCourse/ChevausDeCourse.cs
@@ -13,24 +13,31 @@ class Solution
 {
     static void Main(string[] args)
     {
+        int Temp = int.MaxValue;
+        int Dif = int.MaxValue;
+
+        Console.WriteLine("Nombre chevaux");
         int N = int.Parse(Console.ReadLine());
+        int[] Power = new int[N];
         for (int i = 0; i < N; i++)
         {
-            int pi = int.Parse(Console.ReadLine());
+            Console.WriteLine("puissance");
+            Power[i] = int.Parse(Console.ReadLine());
         }
+        for (int j = 0; j < Power.Length - 1; j++)
+        {
+            Temp = Power[j] - Power[j + 1];
+            Temp = Math.Abs(Temp);
+            if (Temp < Dif)
+            {
+                Dif = Math.Abs(Temp);
+            }
+        }
+        Console.WriteLine(Dif);
+
 
         // Write an answer using Console.WriteLine()
         // To debug: Console.Error.WriteLine("Debug messages...");
 
-        Console.WriteLine("answer");
     }
 }
-//Solution naîve
-//L'algorithme le plus simple consiste simplement à comparer chaque puissance deux à deux puis afficher la différence la plus faible.
-
-//Or, le nombre de valeurs peut atteindre 100 000 chevaux. Les comparer deux à deux nécessiterait plusieurs milliards de comparaisons, ce qui est bien trop pour passer le dernier test.
-
-//Meilleure approche
-//Pour obtenir un score de 100%, il est nécessaire de trouver une astuce permettant d'éviter de faire autant de comparaisons.
-
-//Essayez de répondre à la question suivante : quelle modification sur les données d'entrée vous permettrait de trouver la réponse en parcourant une seule fois les valeurs de puissance (N-1 comparaisons) ?
--- Temperatures/Temperatures.cs
+++ Temperatures/Temperatures.cs
@@ -13,16 +13,27 @@ class Solution
 {
     static void Main(string[] args)
     {
+        int DistanceMin = int.MaxValue;
+        Console.WriteLine("nombre temp");
         int n = int.Parse(Console.ReadLine()); // the number of temperatures to analyse
         string[] inputs = Console.ReadLine().Split(' ');
         for (int i = 0; i < n; i++)
         {
-            int t = int.Parse(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
-        }
+            int t = int.Parse(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526  
 
+            if (Math.Abs(t) < Math.Abs(DistanceMin) || (t == -DistanceMin && DistanceMin < 0))
+            {
+                DistanceMin = int.Parse(inputs[i]);
+
+            }
+        }
+        Console.WriteLine(DistanceMin);
+
+        if (n == 0)
+        {
+            Console.WriteLine();
+        }
         // Write an answer using Console.WriteLine()
         // To debug: Console.Error.WriteLine("Debug messages...");
-
-        Console.WriteLine("result");
     }
 }
